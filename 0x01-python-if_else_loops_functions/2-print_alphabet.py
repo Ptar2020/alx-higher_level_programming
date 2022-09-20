7896a993a8bf2fcba903dceccc726740886476e2
Ptar <peterolwande@gmail.com>
2022-09-20 13:39:56 +0300

    Bit confusion

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+'''
 for a in "abcdefghijklmnopqrstuvwxyz":
-    print('{}'.format(a), end="")
+'''
+for letter in range(ord('a'), ord('z') + 1):
+    print("{:c}" .format(letter), end="")
