d3960d8f7378bbf474d2ced9e280050931ce8baf
Peter Olwande <makonalividza@gmail.com>
2022-09-20 21:14:50 +0300

    Attempt on linked lists

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * *insert_node - Adds node at end of list
+ * @head: The head of the list
+ * @number: The number to be added
+ * 
+ * Return: A pointer to the list
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp;
+
+	if (head != NULL)
+	{
+		temp = malloc(sizeof(listint_t));
+		if (temp == NULL)
+			return (NULL);
+
+		temp->number = strdup(number);
+		temp->len = strlen(number);
+		temp->next = *head;
+
+		*head = temp;
+
+		return (temp);
+	}
+}
--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -73,30 +73,3 @@ void free_listint(listint_t *head)
         free(current);
     }
 }
-/**
- * *insert_node - Adds node at end of list
- * @head: The head of the list
- * @number: The number to be added
- * 
- * Return: A pointer to the list
- */
-
-listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *temp;
-
-	if (head != NULL)
-	{
-		temp = malloc(sizeof(listint_t));
-		if (temp == NULL)
-			return (NULL);
-
-		temp->number = strdup(number);
-		temp->len = strlen(number);
-		temp->next = *head;
-
-		*head = temp;
-
-		return (temp);
-	}
-}
