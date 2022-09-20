b7118febd837af95149d1544d7a107c15e0f164b
Ptar <peterolwande@gmail.com>
2022-09-20 14:17:30 +0300

    dec_to_hex

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -9,26 +9,26 @@ You are not allowed to store numbers or strings in a variable
 You are not allowed to import any module
 """
 
-def dec_to_hex(n):   
-   x = (n % 16)
-   ch = ""
-   if (x < 10):
-       ch = x
-   if (x == 10):
-       ch = "A"
-   if (x == 11):
-       ch = "B"
-   if (x == 12):
-       ch = "ch"
-   if (x == 13):
-       ch = "D"
-   if (x == 14):
-       ch = "E"
-   if (x == 15):
-       ch = "F"
-   if (n - x != 0):
-       return dec_to_hex(n // 16) + str(ch)
-   else:
-       return str(ch)
+def dec_to_hex(n):
+    x = (n % 16)
+    ch = ""
+    if (x < 10):
+        ch = x
+    if (x == 10):
+        ch = "A"
+    if (x == 11):
+        ch = "B"
+    if (x == 12):
+        ch = "ch"
+    if (x == 13):
+        ch = "D"
+    if (x == 14):
+        ch = "E"
+    if (x == 15):
+        ch = "F"
+    if (n - x != 0):
+        return dec_to_hex(n // 16) + str(ch)
+    else:
+        return str(ch)
 for dec in range(0, 99):
     print("{}".format(dec_to_hex(dec)))
