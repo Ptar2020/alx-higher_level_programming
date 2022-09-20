8350d20ee971e7ed3945127272a63e39a0e917b1
Peter Olwande <makonalividza@gmail.com>
2022-09-20 19:09:54 +0300

    last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+"""
+Write a function that prints the last digit of a number.
+
+Prototype: def print_last_digit(number):
+Returns the value of the last digit
+You are not allowed to import any module
+You don’t need to understand __import__
+"""
+
+
+def print_last_digit(number):
+    last = int(repr(number)[-1])
+    print("{}".format(last), end=" ")
+
