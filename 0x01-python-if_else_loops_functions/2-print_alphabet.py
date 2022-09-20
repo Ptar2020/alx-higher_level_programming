25a31b6ee51805d2cb660ba999f521de694192d0
Ptar <peterolwande@gmail.com>
2022-09-20 12:51:39 +0300

    pycode modifications

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""
-Program rints the ASCII alphabet, in lowercase, not followed by a new line.
-
-You can only use one print function with string format
-You can only use one loop in your code
-You are not allowed to store characters in a variable
-You are not allowed to import any module
-"""
-
 for letter in range(ord('a'), ord('z') + 1):
-    print("{:c}" .format(letter), end="")  # default for end='\n'
+    print("{:c}" .format(letter), end="")
+    
