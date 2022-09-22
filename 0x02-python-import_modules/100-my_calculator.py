e2360263d7c946365ffc6991abc9edea40605903
Ptar2020 <peterolwande@gmail.com>
2022-09-22 17:23:30 +0300

    100-my_calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+
+def calculator():
+    args = len(sys.argv[1:])
+    if args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        if sys.argv[2] == "+":
+            add(int(sys.argv[1]), int(sys.argv[3]))
+        elif sys.argv[2] == "-":
+            sub(int(sys.argv[1]), int(sys.argv[3]))
+        elif sys.argv[2] == "/":
+            div(int(sys.argv[1]), int(sys.argv[3]))
+        elif sys.argv[2] == "*":
+            mul(int(sys.argv[1]), int(sys.argv[3]))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+    
+
+if __name__ == "__main__":
+    calculator()
