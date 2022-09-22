73f839077b58ccc8659bef6440f499fa396293b7
Ptar2020 <peterolwande@gmail.com>
2022-09-22 12:51:10 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import sys
-items = sys.argv[1:]
+from sys import argv
+
+
+items = argv[1:]
 count = 0
 if len(items) > 1:
     print(f"{len(items)} arguments:")
@@ -8,6 +10,10 @@ if len(items) > 1:
         count += 1
         print(f"{count}: {item}")
 elif len(items) == 1:
-    print(f"1 argument:\n1: {sys.argv[1]}" )
+    print(f"1 argument:\n1: {argv[1]}" )
 else:
     print(f"{len(items)} arguments.")
+
+
+if __name__ == "__main__":
+    argv
