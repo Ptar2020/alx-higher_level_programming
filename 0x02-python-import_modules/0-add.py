bac107463f1ac8765085b7cedf713e4de949f868
Ptar2020 <peterolwande@gmail.com>
2022-09-22 12:57:56 +0300

    2-args.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    """My addition function
+from add_0 import add
 
-    Args:
-        a (int): first integer
-        b (int): second integer
-    
-    Returns:
-        The return value. a + b
-    """
-    result = a + b
-    return (result)
+
 a = 10
 b = 5
-print(f"1.{a} + {b} = {add(a, b)}")
 print("2.{0} + {1} = {2}".format(a, b, add(a, b)))
