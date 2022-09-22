c069247ef0847cd167fbffa20cb9fe812e4d1611
Ptar <peterolwande@gmail.com>
2022-09-22 05:57:40 -0700

    Resolve Merge conflict in 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -16,18 +16,4 @@ else:
 
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    items = argv[1:]
-    count = 0
-    if len(items) > 1:
-        print(f"{len(items)} arguments:")
-        for item in items:
-            count += 1
-            print(f"{count}: {item}")
-    elif len(items) == 1:
-        print(f"1 argument:\n1: {argv[1]}")
-    else:
-        print(f"{len(items)} arguments.")
-=======
     argv
->>>>>>> 3977c79e079cbf068ee433f4fdd33c08d47b602f
