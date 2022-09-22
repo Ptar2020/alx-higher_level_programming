9eb43f05ca2631cbb7d480d6b22260fcf9d9bb26
Ptar2020 <peterolwande@gmail.com>
2022-09-22 13:27:20 +0300

    Modifications

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,6 +2,7 @@
 from add_0 import add
 
 
-a = 10
-b = 5
-print("2.{0} + {1} = {2}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("2.{0} + {1} = {2}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,18 +2,15 @@
 from sys import argv
 
 
-items = argv[1:]
-count = 0
-if len(items) > 1:
-    print(f"{len(items)} arguments:")
-    for item in items:
-        count += 1
-        print(f"{count}: {item}")
-elif len(items) == 1:
-    print(f"1 argument:\n1: {argv[1]}")
-else:
-    print(f"{len(items)} arguments.")
-
-
 if __name__ == "__main__":
-    argv
+    items = argv[1:]
+    count = 0
+    if len(items) > 1:
+        print(f"{len(items)} arguments:")
+        for item in items:
+            count += 1
+            print(f"{count}: {item}")
+    elif len(items) == 1:
+        print(f"1 argument:\n1: {argv[1]}")
+    else:
+        print(f"{len(items)} arguments.")
