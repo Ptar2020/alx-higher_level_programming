c47873adb5fd7eacb1a1b0820900d57e6ffba0f6
Ptar2020 <peterolwande@gmail.com>
2022-09-23 14:36:51 +0300

    reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,14 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = my_list.reverse()
+    """Reverses a list and prints each item in the list on a new line
+
+    Args:
+        my_list (list):  [].
+    """
+    reversed_list = reversed(my_list)
     for item in reversed_list:
-        print("{}".format(item), end="\n")
+        print("{}".format(item))
 
 
 if __name__ == "__main__":
