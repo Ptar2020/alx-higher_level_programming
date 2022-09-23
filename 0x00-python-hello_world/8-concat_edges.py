0297410e7cd33f4984289591b7871a8bdf4e80dd
Ptar2020 <peterolwande@gmail.com>
2022-09-23 19:16:55 +0300

    Update task 7

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
     language that combines remarkable power with very clear syntax"
-print(f'{str[39:65]} {str[-22:-17]} {str[:6]}')
+print(f'{str[39:66]} {str[-22:-17]}{str[:6]}')
