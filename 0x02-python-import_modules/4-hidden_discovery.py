2bc25c6bbef90d6b95136b706c572f34bef65ee6
Ptar2020 <peterolwande@gmail.com>
2022-09-23 20:52:14 +0300

    Magics

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import hidden_4
+    
+    
+    items = dir(hidden_4)
+    for item in items:
+        if item[:2] != "__":
+            print(item)     
