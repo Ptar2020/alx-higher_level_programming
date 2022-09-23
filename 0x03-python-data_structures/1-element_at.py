b93ef54aefea5e1521ec2a88229b294d5f4844a0
Ptar2020 <peterolwande@gmail.com>
2022-09-23 13:22:04 +0300

    Element at

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        element = my_list[idx]
+        return element
+
+
+if __name__ == "__main__":
+    element_at()
