f0946dd47b79d8e9982cc4ec7b596e8e25db6060
Ptar2020 <peterolwande@gmail.com>
2022-09-23 15:04:21 +0300

    Replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    list_copy = my_list.copy()
+    if idx < 0 or idx > len(my_list):
+        return (list_copy)
+    else:
+        list_copy[idx] = element
+        return (list_copy)
+
+
+if __name__ == "__main__":
+    new_in_list()
