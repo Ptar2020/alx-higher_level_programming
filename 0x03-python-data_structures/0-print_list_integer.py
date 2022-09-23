e217fcf07fb6162be65eac65e8749280ab6746ba
Ptar2020 <peterolwande@gmail.com>
2022-09-23 13:04:41 +0300

    0-print_list_integer

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for item in my_list:
+        print("{}".format(item), end="\n")
+
+
+if __name__ == "__main__":
+    print_list_integer()
