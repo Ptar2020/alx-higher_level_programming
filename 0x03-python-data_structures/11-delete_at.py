c9707403dcdd157175c00a12669149ea09f18c33
Ptar2020 <peterolwande@gmail.com>
2022-09-24 07:36:41 +0300

    Change 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -9,14 +9,14 @@ def delete_at(my_list=[], idx=0):
         idx (int, optional): The index point from which to delete.
 
     Returns:
-        list: new_list if idx is within range or not negative, else my_list
+        list: my_list unchanged if idx is within range or not negative, else my_list changed
     """
-    new_list = my_list.copy()
+    
     if idx < 0 or idx > len(my_list):
         return (my_list)
     else:
-        del new_list[idx]
-        return (new_list)
+        del my_list[idx]
+        return (my_list)
 
 
 if __name__ == "__main__":
