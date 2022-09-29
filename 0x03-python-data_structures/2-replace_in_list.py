736a76280cbb62b00145f544c23cb694157fdd75
Ptar2020 <peterolwande@gmail.com>
2022-09-29 18:56:43 +0300

    Update Tasks

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
-
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return 
     else:
         new_list = my_list[idx] = element
-        return (new_list)
-
-
-if __name__ == "__main__":
-    replace_in_list()
+        return new_list
