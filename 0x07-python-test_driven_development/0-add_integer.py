7302776283d18ba5b0f5a4a4762eee05c4bdb328
Ptar2020 <peterolwande@gmail.com>
2022-10-16 21:43:41 +0300

    Task 0 done

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This module takes two values , gets their sum and returns it
+    Both values must be either float or integer
+    The returned sum is cast to integer
+    Values other than integer or float return TypeError.
+"""
+def add_integer(a, b=98):
+    """Works out the sum of two values and returns it"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
+
