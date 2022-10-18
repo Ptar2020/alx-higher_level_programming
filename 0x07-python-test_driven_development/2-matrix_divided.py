353a2a5c3327524afedba2762bc309b9ba526af5
Ptar2020 <peterolwande@gmail.com>
2022-10-18 20:26:42 +0300

    TDD

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,13 +14,29 @@ Returns a new matrix
 
 
 def matrix_divided(matrix, div):
-    """This function divides all elements in a matrix then returns a new matrix
-
-    Args:
-        matrix (list): The matrix list of lists to be divided
-        div (int): This is the divider
     """
-    if isinstance(matrix, list):
-        for item in matrix:
-            if isinstance(item, (int, float)):
-                raise TypeError()
+    Divides list and Raises TypeError
+    """
+    if not isinstance(div, (int, float)):
+            raise TypeError("div must be a number")
+    elif div is 0:
+            raise ZeroDivisionError("division by zero")
+    typeErr = "matrix must be a matrix (list of lists) of integers/floats"
+    sizeErr = "Each row of the matrix must have the same size"
+    new = []
+    if matrix is None or len(matrix) is 0 or len(matrix[0]) is 0:
+            raise TypeError(typeErr)
+    old = len(matrix[0])
+    for count, y in enumerate(matrix):
+            if not isinstance(y, list):
+                    raise TypeError(typeErr)
+            if len(y) != old:
+                    raise TypeError(sizeErr)
+            old = len(y)
+            new.append(y[:])
+            for a, item in enumerate(y):
+                    if not isinstance(item, (int, float)):
+                            raise TypeError(typeErr)
+                    new[count][a] = round(item / div, 2)
+    else:
+            return (new)
