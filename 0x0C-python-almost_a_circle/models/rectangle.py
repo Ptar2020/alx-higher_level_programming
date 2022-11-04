dfc44a315a5062aa5bdb8e89ce428c8b55315531
Ptar2020 <peterolwande@gmail.com>
2022-11-04 20:46:33 +0300

    Better Finished

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,3 +67,64 @@ class Rectangle(base.Base):
         if type(value) is not int:
             raise TypeError("y must be an integer")
         self.__y = value
+
+    def area(self):
+        """This method returns the area of a rectangle"""
+        return (self.__height * self.__width)
+
+    def display(self):
+        """
+        This function prints in stdout the rectangle
+        """
+        print(("\n" * self.__y) + "\n".join(((" " * self.__x) +
+                                             ("#" * self.__width))
+                                            for a in range(self.__height)))
+
+    def __str__(self):
+        """
+        This function returns [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+        return ("[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".
+                format(self.id, self.__x, self.__y, self.__width,
+                       self.__height))
+
+    def update(self, *args, **kwargs):
+        """
+        function assigns an argument to each attribute
+        """
+        if len(args):
+            for i, j in enumerate(args):
+                if i == 0:
+                    self.id = j
+                if i == 1:
+                    self.width = j
+                if i == 2:
+                    self.height = j
+                if i == 3:
+                    self.x = j
+                if i == 4:
+                    self.y = j
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "height" in kwargs:
+                self.height = kwargs["height"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """
+        This function returns the dictionary representation
+        of a rectangle
+        """
+        dict = {}
+        dict["id"] = self.id
+        dict["width"] = self.width
+        dict["height"] = self.height
+        dict["x"] = self.x
+        dict["y"] = self.y
+        return (dict)
