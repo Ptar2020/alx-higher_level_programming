515d1be2daf72cee92e22a2fb2a9b33142e1f67a
Ptar <peterolwande@gmail.com>
2022-12-20 10:03:05 +0300

    done

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const argv = process.argv;
 if (argv[2] && argv[3]) {
-  console.log(argv[2] + " is " + argv[3]);
+  console.log(argv[2] + ' is ' + argv[3]);
 }
 else if (argv[2] && !argv[3]) {
-  console.log(argv[2] + " is " + argv[3]);
+  console.log(argv[2] + ' is ' + argv[3]);
 }
 else {
-  console.log(argv[2] + " is " + argv[3]);
+  console.log(argv[2] + ' is ' + argv[3]);
 }
