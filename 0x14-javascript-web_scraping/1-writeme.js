05b12ebbee02f45804b1b771980d620ad52ed83a
Ptar2020 <peterolwande@gmail.com>
2023-02-09 21:44:41 +0300

    More edits

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+import { writeFile } from 'fs';
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
     console.log(err);
   }
